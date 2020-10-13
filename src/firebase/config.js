f6cf01a781ac21cd731208ef2828d0e2544de8aa
sebastienlob <lobseb@gmail.com>
2020-10-13 14:34:45 -0400

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYJ-SvLC9ddPZHssJ9nEUjNh_yEJDn5wE",
+    authDomain: "hunt-464c3.firebaseapp.com",
+    databaseURL: "https://hunt-464c3.firebaseio.com",
+    projectId: "hunt-464c3",
+    storageBucket: "hunt-464c3.appspot.com",
+    messagingSenderId: "863561277014",
+    appId: "1:863561277014:web:80f5b391b1017ffa3065f7",
+    measurementId: "G-MVWZQ5V9VG"
+  };
+
+  export default firebaseConfig
